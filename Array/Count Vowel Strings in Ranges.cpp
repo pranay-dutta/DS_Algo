9853a70f5afb67c55bd563f62e24d4f8c6b0ea45
Pranay Dutta <rick15176@gmail.com>
2025-01-03 12:09:12 +0530

    Updated: Count Vowel Strings in Ranges

--- Array/Count Vowel Strings in Ranges.cpp
+++ Array/Count Vowel Strings in Ranges.cpp
@@ -1,20 +1,23 @@
+// Approach 1:
+// TC: O(m+n)
+// SC: O(n)
 class Solution {
-public:
-  vector<int> vowelStrings(vector<string> &words,
-                           vector<vector<int>> &queries) {
+ public:
+  vector<int> vowelStrings(vector<string> &words, vector<vector<int>> &queries) {
     int n = words.size();
-    unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};  // SC: O(5)
+    vector<int> cumSum(n);                                   // SC: O(n)
 
     int count = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {  // TC: O(n)
       if (vowels.count(words[i].front()) && vowels.count(words[i].back())) {
         count++;
       }
       cumSum[i] = count;
     }
 
-    vector<int> ans;
-    for (auto &query : queries) {
+    vector<int> ans;               // O(1) result array
+    for (auto &query : queries) {  // TC: O(m)
       int l = query[0];
       int r = query[1];
 
