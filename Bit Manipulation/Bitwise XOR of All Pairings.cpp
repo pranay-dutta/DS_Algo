7a9f9222aa45e3c85d10ffb0a7479378a737cb9c
Pranay Dutta <rick15176@gmail.com>
2025-01-17 00:18:37 +0530

    Update: Bitwise XOR of All Pairings

--- Bit Manipulation/Bitwise XOR of All Pairings.cpp
+++ Bit Manipulation/Bitwise XOR of All Pairings.cpp
@@ -22,3 +22,29 @@ class Solution {
     return n1Xor ^ n2Xor;
   }
 };
+
+// Approach 2:
+// TC: O(n+m+k) -> O(n)
+// TC: O(k) -> O(n)
+class Solution {
+ public:
+  int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int64_t> mp;  // SC: O(n)
+    int n = nums1.size();
+    int m = nums2.size();
+
+    for (int& x : nums1) {  // TC: O(n)
+      mp[x] += m;
+    }
+    for (int& y : nums2) {  // TC: O(m)
+      mp[y] += n;
+    }
+
+    // Traversing the map and XORing only the odd freq ones
+    int res = 0;
+    for (auto& [x, f] : mp) {  // TC: O(k)
+      if (f & 1) res ^= x;
+    }
+    return res;
+  }
+};
