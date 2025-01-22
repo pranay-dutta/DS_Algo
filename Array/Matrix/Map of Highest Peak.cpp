dd309756516b8ee3ed671e6137caa8393152c573
Pranay Dutta <rick15176@gmail.com>
2025-01-23 01:00:34 +0530

    Update: Map of Highest Peak

--- Array/Matrix/Map of Highest Peak.cpp
+++ Array/Matrix/Map of Highest Peak.cpp
@@ -1,32 +1,69 @@
+// Approach 1:
+// TC: O(m*n)
+// SC: O(m*n)
 class Solution {
  public:
-  vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-    const int r = isWater.size(), c = isWater[0].size();
-    const int d[5] = {0, 1, 0, -1, 0};
-    vector<vector<int>> H(r, vector<int>(c, -1));
-    queue<int> q;
-    for (int i = 0; i < r; i++) {
-      for (int j = 0; j < c; j++) {
-        if (isWater[i][j]) {
-          int idx = (i << 10) + j;
-          H[i][j] = 0;
-          q.push(idx);
+  int m, n;
+  vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};  // R L U D
+
+  void processHeight(vector<vector<int>>& isWater, vector<pair<int, int>>& waterCells) {
+    queue<pair<int, int>> que;                            // SC: O(m*n)
+    vector<vector<bool>> visited(m, vector<bool>(n, 0));  // SC: O(m*n)
+
+    for (auto& [water_i, water_j] : waterCells) {
+      que.push({water_i, water_j});
+      visited[water_i][water_j] = true;
+    }
+
+    que.push({-1, -1});  // level breaker
+    int level = 1;
+
+    while (!que.empty()) {
+      auto [i, j] = que.front();
+      que.pop();
+
+      if (i == -1 && j == -1) {  // level breaker
+        if (!que.empty()) {
+          que.push({-1, -1});
+          level++;
+        }
+        continue;
+      }
+
+      for (auto& dir : directions) {
+        int i_ = i + dir[0];
+        int j_ = j + dir[1];
+
+        if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && !visited[i_][j_]) {
+          visited[i_][j_] = true;
+          isWater[i_][j_] = level;
+          que.push({i_, j_});
         }
       }
     }
-    for (int h = 0; !q.empty(); h++) {
-      int qz = q.size();
-      for (int a = 0; a < qz; a++) {
-        int idx = q.front(), i = idx >> 10, j = idx & ((1 << 10) - 1);
-        q.pop();
-        for (int b = 0; b < 4; b++) {
-          int s = i + d[b], t = j + d[b + 1];
-          if (s < 0 || s >= r || t < 0 || t >= c || H[s][t] != -1) continue;
-          q.push((s << 10) + t);
-          H[s][t] = h + 1;
-        }
-      }
-    }
-    return H;
   }
+  vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+    // Multi source BFS
+
+    m = isWater.size();                 // Row
+    n = isWater[0].size();              // Col
+    vector<pair<int, int>> waterCells;  //SC: O(m*n)
+
+    for (int i = 0; i < m; i++) {  // TC: O(m*n)
+      for (int j = 0; j < n; j++) {
+        if (isWater[i][j]) {
+          isWater[i][j] = 0;
+          waterCells.push_back({i, j});
+        }
+      }
+    }
+    processHeight(isWater, waterCells);  // TC: O(m*n)
+    return isWater;
+  }
+};
+const auto init = []() {
+  ios::sync_with_stdio(false);
+  std::cout.tie(0);
+  std::cin.tie(0);
+  return 1;
 };
