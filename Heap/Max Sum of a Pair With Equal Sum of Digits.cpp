f92a6e5460fa58daf6c7cfc9d3f7275e981ce935
Pranay Dutta <rick15176@gmail.com>
2025-02-12 23:36:27 +0530

    Update: Max Sum of a Pair With Equal Sum of Digits

--- Heap/Max Sum of a Pair With Equal Sum of Digits.cpp
+++ Heap/Max Sum of a Pair With Equal Sum of Digits.cpp
@@ -27,3 +27,39 @@ class Solution {
     return maxSum == 0 ? -1 : maxSum;
   }
 };
+
+// Approach 2:
+// TC: O(n)
+// SC: O(n)
+class Solution {
+ public:
+  int digitSum(int n) {  // O(9)
+    int x = 0;
+    while (n) {
+      x += n % 10;
+      n /= 10;
+    }
+    return x;
+  }
+  int maximumSum(vector<int>& nums) {
+    unordered_map<int, priority_queue<int>> mp;  // set is in decendingorder
+
+    for (int x : nums) {        // O(n)
+      mp[digitSum(x)].push(x);  // O(log n)
+    }
+
+    int maxSum = 0;
+    for (auto [x, maxHeap] : mp) {  //~O(82)
+      if (maxHeap.size() >= 2) {  // if atleast 2 number exist of same digitsum
+        int x = maxHeap.top();
+        maxHeap.pop();
+        int y = maxHeap.top();
+        maxHeap.pop();
+
+        int sum = x + y;
+        maxSum = max(sum, maxSum);
+      }
+    }
+    return maxSum == 0 ? -1 : maxSum;
+  }
+};
