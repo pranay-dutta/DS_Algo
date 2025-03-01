27a1801de27334c631da5c8389954eef9958f0a1
Pranay Dutta <rick15176@gmail.com>
2025-03-01 23:31:44 +0530

    create: Apply Operations to an Array

---
+++ Array/Easy/Apply Operations to an Array.cpp
@@ -0,0 +1,22 @@
+// Approach 1:
+// TC: O(n)
+// SC: O(n)
+class Solution {
+ public:
+  vector<int> applyOperations(vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++) {
+      if (nums[i] == nums[i + 1]) {
+        nums[i] *= 2;
+        nums[i + 1] = 0;
+      }
+    }
+
+    vector<int> res(n, 0);
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+      if (nums[i]) res[j++] = nums[i];
+    }
+    return res;
+  }
+};
